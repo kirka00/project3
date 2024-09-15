535bea81856fefe5dc3739ea0642a3cd72494c47
Kirka <57136540+kirka00@users.noreply.github.com>
2024-09-15 20:46:51 +0300

    Update rendering.c

--- rendering.c
+++ rendering.c
@@ -1,14 +1,17 @@
 #include "rendering.h"
 
 void rendering_graph(char *postfix, int *everythingIsFine) {
-    double coords[MAX_X];
-    for (int k = 0; double x = D_Y_LOWER; x < D_Y_UPPER * M_PI; x += D_Y_UPPER * M_PI / MAX_X, k++) {
-        coords[k] = calculate(postfix, x, everythingIsFine);
+    double coords[MAX_X], stepX = D_Y_UPPER * M_PI / MAX_X, stepY = 2.0 / MAX_Y;
+    
+    // Считаем значения
+    for (int k = 0; k < MAX_X; k++) {
+        coords[k] = calculate(postfix, D_Y_LOWER + k * stepX, everythingIsFine);
     }
-
-    for (double y = LOWER_LIMIT; y <= UPPER_LIMIT; y += 2.0 / MAX_Y) {
+    
+    // Рисуем по высчитанным значеняим
+    for (double y = LOWER_LIMIT; y <= UPPER_LIMIT; y += stepY) {
         for (int x = 0; x < MAX_X; x++) {
-            printf("%c", (coords[x] <= y + 1.0 / MAX_Y && coords[x] >= y - 1.0 / MAX_Y) ? '*' : '.');
+            printf(coords[x] <= y + stepY / 2 && coords[x] >= y - stepY / 2 ? "*" : ".");
         }
         printf("\n");
     }
